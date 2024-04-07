98873cc6e86015d9e70c35be5e7ac3b7d96cd459
Ismael <ismaelbetzoletti@gmail.com>
2024-04-07 16:01:51 -0300

    createHashRouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, HashRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider} from 'react-router-dom';
 import Home from './pages/Home';
 
-const browserRouter = createBrowserRouter([
+const hashRouter = createHashRouter([
   {
     path: "/",
     element: <Home />
@@ -13,9 +13,7 @@ const browserRouter = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <RouterProvider router={browserRouter} />
-    </HashRouter>
+      <RouterProvider router={hashRouter} />
   </React.StrictMode>
 );
 
